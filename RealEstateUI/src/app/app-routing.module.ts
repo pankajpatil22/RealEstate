395e11a44920d99a82f941ad62cc7d7e31b3892f
Pankaj Patil <pankajpatil01998@gmail.com>
2024-03-27 13:32:57 +0530

    login page

--- RealEstateUI/src/app/app-routing.module.ts
+++ RealEstateUI/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: SignInComponent
   }
 ];
 
--- RealEstateUI/src/app/app.module.ts
+++ RealEstateUI/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { HomeModule } from './pages/home/home.module';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     ComponentsModule,
